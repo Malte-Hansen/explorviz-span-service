51f5341cccac78fa19a742f4e33818f05a3b7607
Jan Erik Petersen <JanErikPetersen@web.de>
2023-04-05 04:13:33 +0200

    Add existing kafka avro protocol, adjust kafka application-id

---
+++ src/main/java/net/explorviz/span/injection/SchemaRegistryClientProducer.java
@@ -0,0 +1,26 @@
+package net.explorviz.span.injection;
+
+import io.confluent.kafka.schemaregistry.client.CachedSchemaRegistryClient;
+import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
+import io.quarkus.arc.DefaultBean;
+
+import javax.enterprise.inject.Produces;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+/**
+ * Returns a {@link SchemaRegistryClient} that is used by this application with a maximum number of
+ * 10 schemas.
+ */
+public class SchemaRegistryClientProducer {
+    private static final int MAX_NUM_OF_SCHEMAS = 10;
+
+    @ConfigProperty(name = "quarkus.kafka-streams.schema-registry-url")
+    String schemaRegistryUrl;
+
+    @Produces
+    @DefaultBean
+    public SchemaRegistryClient produceSchemaRegistryClient() {
+        return new CachedSchemaRegistryClient(this.schemaRegistryUrl, MAX_NUM_OF_SCHEMAS);
+    }
+}
---
+++ src/main/java/net/explorviz/span/injection/SerdeProducer.java
@@ -0,0 +1,36 @@
+package net.explorviz.span.injection;
+
+import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import io.quarkus.arc.DefaultBean;
+
+import java.util.Map;
+import javax.enterprise.context.Dependent;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+
+import org.apache.avro.specific.SpecificRecord;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+/**
+ * Returns an injectable {@link SpecificAvroSerde}.
+ */
+@Dependent
+public class SerdeProducer {
+    @Inject
+    SchemaRegistryClient registry;
+
+    @ConfigProperty(name = "quarkus.kafka-streams.schema-registry-url")
+    String schemaRegistryUrl;
+
+    @Produces
+    @DefaultBean
+    public <T extends SpecificRecord> SpecificAvroSerde<T> produceSpecificAvroSerde() {
+        final SpecificAvroSerde<T> valueSerde = new SpecificAvroSerde<>(this.registry);
+        valueSerde.configure(
+            Map.of(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, this.schemaRegistryUrl),
+            false);
+        return valueSerde;
+    }
+}
